99a4f187acfd471908f266a72641985989aac533
vikramdevda753 <student@404-20.sicsr.edu>
2014-12-15 09:45:51 +0530

    evt handling

--- src/com/example/eventhandling/MainActivity.java
+++ src/com/example/eventhandling/MainActivity.java
@@ -1,23 +1,49 @@
 package com.example.eventhandling;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button btnClick;
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        
+        btnClick = (Button) findViewById(R.id.btnClick);
+        
+        OnClickListener listener = new OnClickListener(){
+        	 
+        
+        	
+        	public void onClick(View arg0)
+        	{
+        		btnClick.setText("Button was clicked");
+        		
+        	}
 
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
+        };
+        
+        	btnClick.setOnClickListener((android.view.View.OnClickListener) listener);
+        	
+        	
+        
+    
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
+   
     
 }
